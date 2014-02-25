3948a4d25daca9976f2184e4427cfe409d4e7700
Justin Sternberg <justin@dsgnwrks.pro>
2014-02-24 21:17:02 -0500

    Update to 0.1.1 with better callbacks, one option per term, etc

--- Taxonomy_MetaData.php
+++ Taxonomy_MetaData.php
@@ -3,17 +3,10 @@
 if ( ! class_exists( 'Taxonomy_MetaData' ) ) {
 /**
  * Adds pseudo term meta functionality
- * @version 0.1.0
+ * @version 0.1.1
  * @author  Justin Sternberg
  */
 class Taxonomy_MetaData {
-
-	/**
-	 * Session-cached taxonomy option
-	 * @since  0.1.0
-	 * @var array
-	 */
-	protected static $cat_opts = array();
 
 	/**
 	 * Stores every instance created with this class.
@@ -27,34 +20,91 @@ class Taxonomy_MetaData {
 	 * @since  0.1.0
 	 * @var array
 	 */
-	public $term_meta_fields = array();
+	public $fields = array();
+
+	/**
+	 * Taxonomy Slug
+	 * @since  0.1.0
+	 * @var string
+	 */
+	public $taxonomy = '';
+
+	/**
+	 * Taxonomy Object
+	 * @since  0.1.1
+	 * @var object
+	 */
+	public $tax_object = '';
 
 	/**
 	 * Meta fields heading (optional)
 	 * @since  0.1.0
 	 * @var string
 	 */
-	public $label = '';
+	public $section_title = '';
 
 	/**
 	 * Unique ID string for each taxonomy
 	 * @since  0.1.0
 	 * @var string
 	 */
+	protected $id_base = '';
+
+	/**
+	 * Unique ID string for each taxonomy term
+	 * @since  0.1.0
+	 * @var string
+	 */
 	protected $id = '';
+
+	/**
+	 * Cached option/meta data for this taxonomy
+	 * @since  0.1.0
+	 * @var array
+	 */
+	protected $meta = array();
 
 	/**
 	 * Get Started
 	 * @since  0.1.0
 	 */
-	public function __construct( $taxonomy, $term_meta_fields, $label = '' ) {
-		$this->taxonomy                      = $taxonomy;
-		$this->term_meta_fields              = $term_meta_fields;
-		$this->label                         = $label;
-		$this->id                            = strtolower( __CLASS__ ) . '_' . $this->taxonomy;
-		self::$cat_opts[ $taxonomy ]         = array();
+	public function __construct( $taxonomy, $fields_or_callback, $title = '' ) {
+		if ( isset( self::$taxonomy_objects[ $taxonomy ] ) )
+			return;
+
+		$this->taxonomy      = $taxonomy;
+		$this->id_base       = strtolower( __CLASS__ ) . '_' . $this->taxonomy;
+		$this->fields        = $fields_or_callback;
+		$this->section_title = $title;
+
 		self::$taxonomy_objects[ $taxonomy ] = $this;
-		add_action( 'admin_init', array( $this, 'hooks' )  );
+		add_action( 'admin_init', array( $this, 'hooks' ) );
+
+	}
+
+	/**
+	 * Get fields config array from callback if requested
+	 * @since  0.1.0
+	 * @return array  Fields config array
+	 */
+	public function fields() {
+		return $this->fields;
+	}
+
+	/**
+	 * Loop field array and send through callback function
+	 * @since  0.1.1
+	 * @param  array  $alldata All option data
+	 * @param  mixed  $cb      Callback method/function
+	 */
+	public function loop_fields( $alldata, $cb ) {
+		// Loop through fields and do cb
+		foreach ( $this->fields() as $key => $field ) {
+			$field['id'] = $key;
+			$field = (object) $field;
+
+			call_user_func( $cb, $field, isset( $alldata[ $key ] ) ? $alldata[ $key ] : '' );
+		}
 	}
 
 	/**
@@ -72,7 +122,7 @@ class Taxonomy_MetaData {
 		add_action( 'edited_'. $this->taxonomy, array( $this, 'save_data' ) );
 
 		// Delete it if necessary
-		add_action( 'delete_'. $this->taxonomy, array( $this, 'delete_data' ), 10, 3 );
+		add_action( 'delete_'. $this->taxonomy, array( $this, 'delete_data' ) );
 
 	}
 
@@ -85,75 +135,66 @@ class Taxonomy_MetaData {
 	public function metabox_edit( $term, $taxonomy = '' ) {
 
 		$editpage = isset( $_GET['tag_ID'] ) ? true : false;
-
 		$taxonomy = $taxonomy ? $taxonomy : $term;
+		$term_id  = $editpage ? $term->term_id : 0;
 
-		$tax = get_taxonomy( $taxonomy );
-		if ( !current_user_can( $tax->cap->edit_terms ) )
+		if ( ! current_user_can( $this->taxonomy_object()->cap->edit_terms ) ) {
 			return;
-
-		$opts = $editpage ? $this->get_meta( $term->slug ) : array();
-
-		if ( ! $editpage ) {
-			echo '</table>
-			<style type="text/css">
-			div.form-checkbox {
-				position: relative;
-				padding-bottom: 1.2em;
-				overflow: hidden;
-			}
-			div.form-checkbox label {
-				display: block;
-				position: absolute;
-				top: -1px;
-				left: 25px;
-			}
-			</style>';
 		}
 
-		wp_nonce_field( $this->id.'save_term_meta', $this->id.'save_term_meta' );
+		// Initiate ID
+		$this->id( $term_id );
+		// Get term meta
+		$data = get_option( $this->id() );
 
-		if ( $this->label )
-			printf( '<h3>%s</h3>', $this->label );
+		// Add a title for these fields, if requested
+		if ( $this->section_title ) : ?>
+		<h3 class="cmb_metabox_title"><?php echo $this->section_title; ?></h3>
+		<?php endif; ?>
+		<input type="hidden" name="term_opt_name" value="<?php echo $this->id( $term_id ); ?>">
+		<?php wp_nonce_field( 'term_meta_box_nonce', 'term_meta_box_nonce', false, true ); ?>
+		<table class="form-table term-meta-box">
+			<?php
+			// Loop through fields and do field view
+			$this->loop_fields( $data, array( $this, 'render_field_view' ) );
+			?>
+		</table>
+		<?php
+	}
 
-		echo '<table class="form-table"><tbody>';
+	/**
+	 * Field view
+	 * @since  0.1.1
+	 * @param  array  $field Field config
+	 * @param  mixed  $data  Field value
+	 */
+	public function render_field_view( $field, $data ) {
+		$cb = isset( $field->render_cb ) && is_callable( $field->render_cb )
+			? $field->render_cb
+			: array( $this, 'text_input_view' );
+		$id = '_id_'. sanitize_html_class( $field->id );
+		?>
+		<tr id="<?php echo $id; ?>">
+			<th>
+				<label for="<?php echo $field->id; ?>"><?php echo $field->name; ?></label>
+			</th>
+			<td>
+				<?php call_user_func( $cb, $field, $data, $field->id ); ?>
+			</td>
+		</tr>
+		<?php
+	}
 
-		foreach ( $this->term_meta_fields as $metakey => $metainfo ) {
-
-			$val = isset( $opts[ $metakey ] ) ? $opts[ $metakey ] : '';
-			$placeholder = '';
-
-			// If we want to display a placeholder
-			if ( isset( $metainfo['placeholder'] ) ) {
-
-				$placeholder = $metainfo['placeholder'] === true
-					? $this->get_meta( 'placeholder', $metakey )
-					: $metainfo['placeholder'];
-			}
-
-			$id = $this->id . $metakey;
-			$type = isset( $metainfo['type'] ) ? $metainfo['type'] : 'text" size="40" placeholder="'. esc_attr( $placeholder );
-			$val = $type == 'checkbox' ? 1 : $val;
-			$class = $type == 'checkbox' ? 'form-checkbox' : 'form-field';
-
-			echo ( $editpage ) ? '<tr class="'. $class .'">' : '<div class="'. $class .'">';
-				echo ( $editpage ) ? '<th scope="row" valign="top">' : '';
-					echo '<label for="'. $id .'">'. $metainfo['label'] .'</label>';
-				echo ( $editpage ) ? '</th><td>' : '';
-					echo '<input name="'. $this->id .'['. $metakey .']" id="'. $id .'" type="'. $type .'" value="'. esc_attr( $val ) .'"';
-					if ( $type == 'checkbox' )
-						checked( isset( $opts[ $metakey ] ) );
-					echo '/>';
-					echo isset( $metainfo['desc'] )
-						? '<p class="description">'. $metainfo['desc'] .'</p>'
-						: '';
-					echo ( $editpage ) ? '</td>' : '';
-			echo ( $editpage ) ? '</tr>' : '</div>';
-
-		}
-
-		echo '</tbody></table>';
-
+	/**
+	 * Default field input view
+	 * @since  0.1.1
+	 * @param  array  $field Field config
+	 * @param  mixed  $data  Field value
+	 */
+	public function text_input_view( $field, $data ) {
+		echo '<input type="text" class="regular-text" name="', $field->id, '" id="', $field->id, '" value="', esc_attr( $data ), '" />';
+		if ( isset( $field->desc ) && $field->desc )
+			echo "\n<p class=\"description\">{$field->desc}</p>\n";
 	}
 
 	/**
@@ -163,175 +204,119 @@ class Taxonomy_MetaData {
 	 */
 	public function save_data( $term_id ) {
 
-		// Make sure we have the right priveleges
 		if (
-			!isset( $_POST[$this->id.'save_term_meta'] )
-			|| ! isset( $_POST['taxonomy'] )
 			// check nonce
-			|| ! wp_verify_nonce( $_POST[$this->id.'save_term_meta'], $this->id.'save_term_meta' )
+			! isset( $_POST['term_opt_name'], $_POST['term_meta_box_nonce'], $_POST['action'] )
+			|| ! wp_verify_nonce( $_POST['term_meta_box_nonce'], 'term_meta_box_nonce' )
 		)
 			return;
 
 		// Can the user edit this term?
-		if ( ! ( $tax = get_taxonomy( $_POST['taxonomy'] ) ) || ! current_user_can( $tax->cap->edit_terms ) )
+		if ( ! current_user_can( $this->taxonomy_object()->cap->edit_terms ) )
 			return;
 
-		if ( ! ( $term = get_term( $term_id, $_POST['taxonomy'] ) ) )
-			return;
+		$this->id = ( false !== strpos( $_POST['term_opt_name'], 'setme' ) )
+			? $this->id = $this->id_base .'_'. $term_id
+			: $this->id( $term_id );
 
-		$opts = get_option( $this->id );
+		$this->sanitized = array();
 
-		if ( ! isset( $_POST[ $this->id ] ) || ! is_array( $_POST[ $this->id ] ) ) {
-			unset( $opts[ $term->slug ] );
-			update_option( $this->id, $opts );
-			return;
+		// Loop and sanitize data
+		$this->loop_fields( $_POST, array( $this, 'sanitize_field' ) );
+
+		// Save the field data
+		update_option( $this->id(), $this->sanitized );
+	}
+
+	/**
+	 * Sanitizes an input field
+	 * @since  0.1.1
+	 * @param  array  $field Field config
+	 * @param  mixed  $data  Field value
+	 */
+	public function sanitize_field( $field, $data ) {
+		$cb = isset( $field->sanitize_cb ) && is_callable( $field->sanitize_cb ) ? $field->sanitize_cb : 'sanitize_text_field';
+		$this->sanitized[ $field->id ] = call_user_func( $cb, $data );
+	}
+
+	/**
+	 * Generate option key id
+	 * @since  0.1.0
+	 * @param  integer $term_id Optional, Term ID
+	 * @return string           Option key
+	 */
+	public function id( $term_id = 0 ) {
+
+		if ( ! $this->id ) {
+			$this->id = $term_id ? $this->id_base .'_'. $term_id : $this->id_base . '_setme';
 		}
 
-		foreach ( $_POST[ $this->id ] as $metakey => $val ) {
+		return $this->id;
+	}
 
-			// Sanitize value and add to our options array
-			$opts[ $term->slug ][ $metakey ] = $this->sanitize( $metakey, $val );
-		}
-		// OK, save it
-		update_option( $this->id, $opts );
+	/**
+	 * Retrieves the full db object for this instance' taxonomy
+	 * @since  0.1.1
+	 * @return object  Taxonomy object
+	 */
+	public function taxonomy_object() {
+		if ( $this->tax_object )
+			return $this->tax_object;
+
+		$this->tax_object = get_taxonomy( $this->taxonomy );
+		return $this->tax_object;
 	}
 
 	/**
 	 * Remove associated term meta when deleting a term
 	 * @since  0.1.0
 	 * @param int $term_id      Term's ID
-	 * @param int $tt_id        Term Taxonomy ID
-	 * @param obj $deleted_term Deleted term object
 	 */
-	public function delete_data( $term, $tt_id, $deleted_term ) {
-		$opts = get_option( $this->id );
-		unset( $opts[ $deleted_term->slug ] );
-		update_option( $this->id, $opts );
+	public function delete_data( $term_id ) {
+		return delete_option( $this->id( $term_id ) );
 	}
 
 	/**
-	 * Delete meta for whole taxonomy all term meta, or specific meta field for term
+	 * Returns the $this->taxonomy site option for this term ID
 	 * @since  0.1.0
-	 * @param  string $taxonomy  Taxonomy slug
-	 * @param  string $term_slug The slug of the term whose option we're getting
-	 * @param  string $key       Term meta key to check
-	 * @return bool              False if option was not updated or true
+	 * @param  string  $term_id  The term id for the options we're getting
+	 * @return mixed             Option value
 	 */
-	public static function delete( $taxonomy, $term_slug = '', $key = '' ) {
-		// Get taxonomy instance
-		if ( ! ( $instance = self::get_instance( $taxonomy ) ) )
-			return false;
+	public function _get_meta( $term_id ) {
+		if ( isset( $this->meta[ $term_id ] ) )
+			return $this->meta[ $term_id ];
 
-		// If no term slug, delete entire option
-		if ( ! $term_slug ) {
-			return delete_option( $instance->id );
-		}
-
-		$opts = get_option( $instance->id );
-
-		// If no term meta keay, delete all the term's meta
-		if ( ! $key ) {
-			unset( $opts[ $term_slug ] );
-			return update_option( $instance->id, $opts );
-		}
-
-		// Delete just this term key's meta
-		unset( $opts[ $term_slug ][ $key ] );
-		return update_option( $instance->id, $opts );
-
+		$this->meta[ $term_id ] = get_option( $this->id( $term_id ) );
+		return $this->meta[ $term_id ];
 	}
 
 	/**
-	 * Set associated term meta when deleting a term
+	 * Returns term meta with options to return a subset
 	 * @since  0.1.0
-	 * @param  string $taxonomy  Taxonomy slug
-	 * @param  string $term_slug The slug of the term whose option we're getting
-	 * @param  string $key       Term meta key to check
-	 * @param  mixed  $val       Term meta value to set
-	 * @return bool              False if option was not updated or true
+	 * @param  string  $term_id  The term id for the options we're getting
+	 * @param  string  $key      Term meta key to check
+	 * @return mixed             Requested value | false
 	 */
-	public static function set( $taxonomy, $term_slug, $key, $value ) {
-		// Get taxonomy instance
-		if ( ! ( $instance = self::get_instance( $taxonomy ) ) )
-			return false;
-
-		$opts = get_option( $instance->id );
-
-		// Sanitize value and add to our options array
-		$opts[ $term_slug ][ $key ] = $instance->sanitize( $key, $value );
-
-		// OK, save it
-		update_option( $instance->id, $opts );
-	}
-
-	/**
-	 * Checks for a sanitization callback or defaults to sanitizing via sanitize_text_field
-	 * @since  0.1.0
-	 * @param  string  $metakey Term meta key to check
-	 * @param  mixed   $val     Term meta value to sanitize
-	 * @return mixed            Sanitized value
-	 */
-	public function sanitize( $metakey, $val ) {
-		$sanitize = isset( $this->term_meta_fields[ $metakey ]['sanitize'] ) ? $this->term_meta_fields[ $metakey ]['sanitize'] : 'sanitize_text_field';
-
-		if ( method_exists( $this, $sanitize ) )
-			return call_user_func( array( $this, $sanitize ), trim( $val ) );
-
-		return call_user_func( $sanitize, trim( $val ) );
-	}
-
-	/**
-	 * Sanitizes to a boolean
-	 * @since  0.1.0
-	 * @param  mixed  $data Data to sanitize
-	 * @return bool         Ristricted to a boolean
-	 */
-	public function parse_boolean( $data ) {
-		return filter_var( $data, FILTER_VALIDATE_BOOLEAN );
-	}
-
-	/**
-	 * Returns the $this->taxonomy site option with options to return a subset
-	 * @since  0.1.0
-	 * @param  string  $term_slug  The slug of the term whose option we're getting
-	 * @param  string  $key        Term meta key to check
-	 * @return mixed               Requested value | false
-	 */
-	public function get_meta( $term_slug = '', $key = '' ) {
-
-		self::$cat_opts[ $this->taxonomy ] = ! empty( self::$cat_opts[ $this->taxonomy ] ) ? self::$cat_opts[ $this->taxonomy ] : (array) get_option( $this->id );
-
-		if ( empty( self::$cat_opts[ $this->taxonomy ] ) )
-			return false;
-
-		if ( ! $term_slug )
-			return self::$cat_opts[ $this->taxonomy ];
-
-		if ( ! $key )
-			return is_array( self::$cat_opts[ $this->taxonomy ] ) && array_key_exists( $term_slug, self::$cat_opts[ $this->taxonomy ] ) ? self::$cat_opts[ $this->taxonomy ][ $term_slug ] : false;
-
-		if (
-			array_key_exists( $term_slug, self::$cat_opts[ $this->taxonomy ] )
-			&& array_key_exists( $key, self::$cat_opts[ $this->taxonomy ][ $term_slug ] )
-		)
-			return self::$cat_opts[ $this->taxonomy ][ $term_slug ][ $key ];
-
-		return false;
+	public function get_meta( $term_id, $key = '' ) {
+		$data = $this->_get_meta( $term_id );
+		if ( $key )
+			return isset( $data[ $key ] ) ? $data[ $key ] : false;
+		return $data;
 	}
 
 	/**
 	 * Public method for getting term meta
 	 * @since  0.1.0
-	 * @param  string $taxonomy  Taxonomy slug
-	 * @param  string $term_slug The slug of the term whose option we're getting
-	 * @param  string $key       Term meta key to check
-	 * @return mixed             Requested value | false
+	 * @param  string $taxonomy Taxonomy slug
+	 * @param  string $term_id  The ID of the term whose option we're getting
+	 * @param  string $key      Term meta key to check
+	 * @return mixed            Requested value | false
 	 */
-	public static function get( $taxonomy, $term_slug = '', $key = '' ) {
+	public static function get( $taxonomy, $term_id = '', $key = '' ) {
 		// Get taxonomy instance
 		$instance = self::get_instance( $taxonomy );
 		// Return the meta, or false if the taxonomy object doesn't exist
-		return $instance ? $instance->get_meta( $term_slug, $key ) : false;
+		return $instance ? $instance->get_meta( $term_id, $key ) : false;
 	}
 
 	/**
@@ -346,6 +331,15 @@ class Taxonomy_MetaData {
 			return false;
 		// Ok, send it back.
 		return self::$taxonomy_objects[ $taxonomy ];
+	}
+
+	/**
+	 * Public method for getting all instanciated instances of this class
+	 * @since  0.1.0
+	 * @return array Array of Taxonomy_MetaData instances
+	 */
+	public static function get_all_instances() {
+		return self::$taxonomy_objects;
 	}
 
 }
